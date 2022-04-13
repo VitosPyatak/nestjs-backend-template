bcf2c6784d2a8c543b344e3a8325d83d7e25dea6
vic <victor.pyatak@botscrew.com>
2022-04-13 15:07:19 +0300

    develop: [refactor] removing unstable components

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RedisCacheModule } from './cache/cache.module';
+
 import { HttpRequestModule } from './http-request/http-request.module';
 import { MongoModule } from './mongo/mongo.module';
 import { ScheduleTaskModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [ScheduleTaskModule, MongoModule, HttpRequestModule, RedisCacheModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScheduleTaskModule, MongoModule, HttpRequestModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
